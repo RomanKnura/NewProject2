da9bc0872a66de9c6a89ac89b982a4b50a7da002
Roman Knura <roman.knura@dpdireland.ie>
2016-03-25 11:05:26 +0100

    Task1 - first commit

--- newProject2/src/main/java/ie/dpd/newproject2/NewClass.java
+++ newProject2/src/main/java/ie/dpd/newproject2/NewClass.java
@@ -10,5 +10,7 @@ package ie.dpd.newproject2;
  * @author roman.knura
  */
 public class NewClass {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
 }
